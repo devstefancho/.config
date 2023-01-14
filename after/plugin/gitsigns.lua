03ef9406583dcd77409306951ffc6682270a18ac
devstefancho <stefancho@joseongjins-MacBook-Pro.local>
2023-01-14 17:08:44 +0900

    show current line git blames

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -3,4 +3,12 @@ if not status then
 	return
 end
 
-gitsigns.setup()
+gitsigns.setup({
+	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 100,
+		ignore_whitespace = false,
+	},
+})
