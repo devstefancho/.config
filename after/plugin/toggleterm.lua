17cedabce883fe46830cf5d2f3b70bc28db86ca1
devstefancho <stefancho@joseongjins-MacBook-Pro.local>
2023-01-15 19:03:20 +0900

    add lazygit for toggle terminal

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -12,10 +12,23 @@ t.setup({
 })
 
 function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	keymap("t", "<esc>", [[<C-\><C-n>]], opts)
-	keymap("t", "jk", [[<C-\><C-n>]], opts)
+	keymap("t", "<ESC>", [[<C-\><C-n>]], { buffer = 0 })
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
+-- Use Lazygit
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, count = 5 })
+
+function _lazygit_toggle()
+	lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>lg",
+	"<cmd>lua _lazygit_toggle()<CR>",
+	{ noremap = true, silent = true, desc = "Show [L]azy [G]it" }
+)
