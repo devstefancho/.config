55ade8ab501b961ff6f4b7d4503f1070aad9baad
Steven <sj_cho@fitpet.co.kr>
2023-01-20 00:36:53 +0900

    Feat: install nvim-tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -81,4 +81,13 @@ packer.startup(function(use)
 			require("toggleterm").setup()
 		end,
 	})
+
+	-- File Explorer
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons", -- optional, for file icons
+		},
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	})
 end)
