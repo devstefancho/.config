8c8873dc25010c0cbf95d1bd7440ddb064a4a6b8
devstefancho <skku7714@gmail.com>
2023-01-23 14:14:04 +0900

    🐛 fix telescope current_buffer_fuzzy_find

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -44,7 +44,7 @@ packer.startup(function(use)
   use("windwp/nvim-ts-autotag")
 
   -- Telescope
-  use({ "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } })
+  use({ "nvim-telescope/telescope.nvim", tag = "0.1.1", requires = { { "nvim-lua/plenary.nvim" } } })
   use("nvim-telescope/telescope-file-browser.nvim")
 
   -- File Icons
