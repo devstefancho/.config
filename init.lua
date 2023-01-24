960beeaa60f6ea8d4fb19664e385c56a2bf69f68
devstefancho <skku7714@gmail.com>
2023-01-25 01:20:15 +0900

    Refactor: move files

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
-require("base")
-require("highlights")
-require("maps")
-require("plugins")
+require("devstefancho.base")
+require("devstefancho.highlights")
+require("devstefancho.maps")
+require("devstefancho.plugins")
 
 -- Copy to Clipboard
 local has = function(x)
-	return vim.fn.has(x) == 1
+  return vim.fn.has(x) == 1
 end
 
-local is_mac = has("macunix")
-local is_win = has("win32")
+local is_mac = has("devstefancho.macunix")
+local is_win = has("devstefancho.win32")
 
 if is_mac then
-	require("macos")
+  require("devstefancho.macos")
 end
 if is_win then
-	require("windows")
+  require("devstefancho.windows")
 end
