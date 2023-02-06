ec82349e650c4eb72a65acd90e06d465ccc5303d
devstefancho <skku7714@gmail.com>
2023-02-06 14:55:43 +0900

    Add: Treesitter incremental selection

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -28,4 +28,13 @@ ts.setup({
   context_commentstring = {
     enable = true,
   },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<c-space>",
+      node_incremental = "<c-space>",
+      scope_incremental = "<c-s>",
+      node_decremental = "grm",
+    },
+  },
 })
