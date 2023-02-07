219f165416ca2529fb564bb361b81b4d2e47cf99
devstefancho <skku7714@gmail.com>
2023-02-07 23:21:14 +0900

    Add: change lspsaga default keymap

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -7,4 +7,16 @@ lspsaga.setup({
   symbol_in_winbar = {
     enable = false,
   },
+  finder = {
+    max_height = 0.5,
+    keys = {
+      jump_to = "p",
+      edit = { "o", "<CR>" },
+      vsplit = "<c-v>", -- same as telescope vsplit keymap
+      split = { "i", "<c-x>" }, -- same as telescope split keymap
+      tabe = { "t", "<c-t>" }, -- same as telescope split keymap
+      quit = { "q", "<ESC>" },
+      close_in_preview = "<ESC>",
+    },
+  },
 })
