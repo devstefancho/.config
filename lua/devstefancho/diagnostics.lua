c26d98b69abf3ad3897ba961f6b4c1403ce67afd
devstefancho <skku7714@gmail.com>
2023-02-26 20:01:45 +0900

    Update: Diagnostic config

--- lua/devstefancho/diagnostics.lua
+++ lua/devstefancho/diagnostics.lua
@@ -1,2 +1,6 @@
 -- Hide virtual inline diagnostic message
-vim.diagnostic.config({ virtual_text = false })
+vim.diagnostic.config({
+  virtual_text = { severity = "ERROR" },
+  underline = { severity = "WARN" },
+  signs = true,
+})
