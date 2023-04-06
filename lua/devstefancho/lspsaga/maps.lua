32ca8e73e04bffe7ff4723981ebecbc528c174e2
Steven <sj_cho@fitpet.co.kr>
2023-04-06 09:50:23 +0900

    Keymap: change diagnostic keymap

--- lua/devstefancho/lspsaga/maps.lua
+++ lua/devstefancho/lspsaga/maps.lua
@@ -9,8 +9,8 @@ end
 
 -- Diagnostic
 keymap("<leader>do", "<Cmd>Lspsaga show_line_diagnostics<CR>", "[D]iagnostics [O]pen") -- same as vim.diagnostic.open_float
-keymap("<leader>dp", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "[d]iagnostic [p]rev") -- same as vim.diagnostic.goto_prev
-keymap("<leader>dn", "<Cmd>Lspsaga diagnostic_jump_next<CR>", "[d]iagnostic [n]ext") -- same as vim.diagnostic.goto_next
+keymap("[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", "[d]iagnostic [p]rev") -- same as vim.diagnostic.goto_prev
+keymap("]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>", "[d]iagnostic [n]ext") -- same as vim.diagnostic.goto_next
 
 -- Definition
 keymap("<leader>gd", "<Cmd>Lspsaga lsp_finder<CR>", "[g]o to [d]efinition of usages") -- Show usages
