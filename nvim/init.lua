1f59bd7aba38ef11cad5b6af25258e95610a0bfa
devstefancho <skku7714@gmail.com>
2023-09-03 00:40:08 +0900

    Feature: automatically updating wiki's updatedDate
    - Reference: https://johngrib.github.io/wiki/vimwiki/

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,4 +42,36 @@ require("lazy").setup("devstefancho.plugins")
 if vim.fn.has("gui_vimr") == 1 then
   -- set vimr cwd to ~/Vault
   vim.cmd("cd ~/Vault")
+
+  function LastModified()
+    -- Check if the global variable g:md_modify_disabled is set
+    if vim.g.md_modify_disabled then
+      return
+    end
+
+    -- Check if the current buffer is modified
+    if vim.bo.modified then
+      -- Save the current cursor position
+      local save_cursor = vim.fn.getpos(".")
+
+      -- Determine the range for the search and replace
+      local n = math.min(10, vim.fn.line("$"))
+
+      -- Update the "updated :" timestamp
+      for i = 1, n do
+        local line = vim.fn.getline(i)
+        if line:match("^updatedDate:") then
+          local updated_line = line:gsub("^updatedDate:.*", "updatedDate: " .. os.date("%Y-%m-%d"))
+          vim.api.nvim_buf_set_lines(0, i - 1, i, false, { updated_line })
+          print("updatedDate updated!")
+          break
+        end
+      end
+
+      -- Restore the cursor position
+      vim.fn.setpos(".", save_cursor)
+    end
+  end
+
+  vim.api.nvim_command("autocmd BufWritePre *.md lua LastModified()")
 end
