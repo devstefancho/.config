d14a9b83a6c40bb10bf7fc665a20738ec9638a2f
devstefancho <skku7714@gmail.com>
2023-09-28 01:05:26 +0900

    feat: add catppuccin theme, remove other themes

--- nvim/lua/devstefancho/plugins/bufferline.lua
+++ nvim/lua/devstefancho/plugins/bufferline.lua
@@ -2,32 +2,43 @@ return {
   {
     "akinsho/bufferline.nvim",
     version = "v3.*",
-    dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
     config = function()
+      -- mocha colors {https://github.com/catppuccin/nvim/blob/main/lua/catppuccin/palettes/mocha.lua}
+      local mocha = require("catppuccin.palettes").get_palette("mocha")
+
       require("bufferline").setup({
         options = {
-          mode = "tabs",
+          mode = "buffers",
           separator_style = "thin",
           show_close_icon = false,
           show_buffer_close_icons = false,
           color_icons = true,
           truncate_names = false,
           diagnostics = "nvim_lsp",
-          name_formatter = function(buf)
-            return buf.path -- show full path in bufferline
-          end,
+          -- name_formatter = function(buf)
+          --   return buf.path -- show full path in bufferline
+          -- end,
         },
         -- fg is font-color, bg is background-color
-        highlights = {
-          separator = {},
-          separator_selected = {},
-          background = { -- inactive buffers
+        highlights = require("catppuccin.groups.integrations.bufferline").get({
+          styles = { "italic", "bold" },
+          custom = {
+            all = {
+              fill = { bg = mocha.base }, -- background color for rest area of bufferline
+            },
+            mocha = {
+              background = {
+                fg = mocha.surface1, -- inactive buffer font color
+              },
+            },
+            latte = {
+              background = { fg = "#000000" },
+            },
           },
-          buffer_selected = {
-            bold = true,
-          },
-          fill = {},
-        },
+        }),
       })
     end,
   },
--- nvim/lua/devstefancho/plugins/lualine.lua
+++ nvim/lua/devstefancho/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
     require("lualine").setup({
       options = {
         icons_enabled = true,
-        theme = "tokyonight",
+        theme = "catppuccin",
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
         disabled_filetypes = {},
