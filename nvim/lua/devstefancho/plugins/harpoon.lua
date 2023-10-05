91fd1b15e95a52a051c62f646d098010f118ff1e
devstefancho <skku7714@gmail.com>
2023-10-06 00:47:27 +0900

    chore: add harpoon keymap for switching buffer

--- nvim/lua/devstefancho/plugins/harpoon.lua
+++ nvim/lua/devstefancho/plugins/harpoon.lua
@@ -7,11 +7,17 @@ return {
     config = function()
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
-
       local keymap = require("devstefancho.utils").createKeymap("Harpoon")
 
       keymap("<leader>a", mark.add_file, "[a]dd a file")
       keymap("<C-e>", ui.toggle_quick_menu, "[e]xplore files")
+
+      keymap("<C-y>", function()
+        ui.nav_file(1)
+      end)
+      keymap("<C-g>", function()
+        ui.nav_file(2)
+      end)
     end,
   },
 }
