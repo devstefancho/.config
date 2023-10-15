598abcb52dcd8d98dd2b7512fe96909f0063ea0f
devstefancho <skku7714@gmail.com>
2023-10-15 21:27:40 +0900

    feat: upgrade plugins and fix indent_blankline config

--- nvim/lua/devstefancho/plugins/indent-blankline.lua
+++ nvim/lua/devstefancho/plugins/indent-blankline.lua
@@ -1,3 +1,14 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    local ibl = require("ibl")
+    local icons = require("devstefancho.icons")
+
+    ibl.setup({
+      indent = {
+        char = icons.ui.LineLeft,
+      },
+    })
+  end,
 }
