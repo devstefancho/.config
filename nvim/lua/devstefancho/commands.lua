7820932c38c85b93d0ed7dccac9e6b2716aac23b
devstefancho <skku7714@gmail.com>
2023-10-29 02:31:33 +0900

    plugin: git worktree

--- nvim/lua/devstefancho/commands.lua
+++ nvim/lua/devstefancho/commands.lua
@@ -8,6 +8,17 @@ vim.cmd([[
 command! MyJiraCommands lua require('devstefancho.telescope').my_jira_commands()
 ]])
 
+vim.cmd([[
+command! GitworktreeCreate lua require('telescope').extensions.git_worktree.create_git_worktree() 
+]])
+
+-- <Enter> - switches to that worktree
+-- <c-d> - deletes that worktree
+-- <c-f> - toggles forcing of the next deletion
+vim.cmd([[
+command! GitworktreeSwitch lua require('telescope').extensions.git_worktree.git_worktrees()
+]])
+
 -- Auto Commands for Fold
 vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
   pattern = { "*.*" },
--- nvim/lua/devstefancho/plugins/git.lua
+++ nvim/lua/devstefancho/plugins/git.lua
@@ -1,53 +1,91 @@
 local keymap = require("devstefancho.utils").createKeymap("GitSigns")
 
 return {
-  "lewis6991/gitsigns.nvim", -- tag = 'release' -- To use the latest release (do not use this if you build Neovim nightly or dev builds!)
-  config = function()
-    require("gitsigns").setup({
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 300,
-        ignore_whitespace = false,
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+  {
+    "lewis6991/gitsigns.nvim", -- tag = 'release' -- To use the latest release (do not use this if you build Neovim nightly or dev builds!)
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 300,
+          ignore_whitespace = false,
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
 
-        -- Navigation
-        keymap("]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, "go to next hunk")
+          -- Navigation
+          keymap("]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, "go to next hunk")
 
-        keymap("[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, "go to prev hunk")
+          keymap("[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, "go to prev hunk")
 
-        -- Actions
-        keymap("<leader>ghs", ":Gitsigns stage_hunk<CR>", "[h]unk [s]taged of current line")
-        keymap("<leader>ghr", ":Gitsigns reset_hunk<CR>", "[h]unk [r]eset")
-        keymap("<leader>ghS", gs.stage_buffer, "all [h]unks in buffer [S]taged")
-        keymap("<leader>ghu", gs.undo_stage_hunk, "last [h]unk [u]ndo stage")
-        keymap("<leader>ghR", gs.reset_buffer, "all [h]unks in buffer [R]eset")
-        keymap("<leader>ghp", gs.preview_hunk, "[h]unk [p]review")
-        keymap("<leader>ghb", function()
-          gs.blame_line({ full = true })
-        end, "[h]unk [b]lame line")
-        keymap("<leader>ghd", gs.diffthis, "[h]unk [d]iff this")
-      end,
-    })
-  end,
-  cond = require("devstefancho.plugins_status").plugins_status["git"],
+          -- Actions
+          keymap("<leader>ghs", ":Gitsigns stage_hunk<CR>", "[h]unk [s]taged of current line")
+          keymap("<leader>ghr", ":Gitsigns reset_hunk<CR>", "[h]unk [r]eset")
+          keymap("<leader>ghS", gs.stage_buffer, "all [h]unks in buffer [S]taged")
+          keymap("<leader>ghu", gs.undo_stage_hunk, "last [h]unk [u]ndo stage")
+          keymap("<leader>ghR", gs.reset_buffer, "all [h]unks in buffer [R]eset")
+          keymap("<leader>ghp", gs.preview_hunk, "[h]unk [p]review")
+          keymap("<leader>ghb", function()
+            gs.blame_line({ full = true })
+          end, "[h]unk [b]lame line")
+          keymap("<leader>ghd", gs.diffthis, "[h]unk [d]iff this")
+        end,
+      })
+    end,
+    cond = require("devstefancho.plugins_status").plugins_status.gitsigns,
+  },
+  -- Worktree
+  {
+    "ThePrimeagen/git-worktree.nvim",
+    config = function()
+      require("git-worktree").setup({
+        change_directory_command = "cd",
+        update_on_change = true,
+        update_on_change_command = "e .",
+        clearjumps_on_change = true,
+        autopush = false,
+      })
+      require("telescope").load_extension("git_worktree")
+
+      local Worktree = require("git-worktree")
+
+      -- op = Operations.Switch, Operations.Create, Operations.Delete
+      -- metadata = table of useful values (structure dependent on op)
+      --      Switch
+      --          path = path you switched to
+      --          prev_path = previous worktree path
+      --      Create
+      --          path = path where worktree created
+      --          branch = branch name
+      --          upstream = upstream remote name
+      --      Delete
+      --          path = path where worktree deleted
+
+      Worktree.on_tree_change(function(op, metadata)
+        if op == Worktree.Operations.Switch then
+          print("Switched from " .. metadata.prev_path .. " to " .. metadata.path)
+          -- os.execute("yarn") -- example for running yarn in new worktree
+        end
+      end)
+    end,
+    cond = require("devstefancho.plugins_status").plugins_status["git-worktree"],
+  },
 }
