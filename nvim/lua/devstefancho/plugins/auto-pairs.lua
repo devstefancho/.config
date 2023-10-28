e278c534eb52dbd80747fdcf80ae3bce5fd44f25
devstefancho <skku7714@gmail.com>
2023-10-29 02:16:35 +0900

    chore: update plugin status

--- nvim/lua/devstefancho/plugins/auto-pairs.lua
+++ nvim/lua/devstefancho/plugins/auto-pairs.lua
@@ -1,13 +1,13 @@
 return {
   {
     "windwp/nvim-autopairs",
-    cond = require("devstefancho.plugins_status").plugins_status["autopairs"],
+    cond = require("devstefancho.plugins_status").plugins_status.autopairs,
   },
   {
     "windwp/nvim-ts-autotag",
     opts = {
       enable_close_on_slash = false, -- slash bug {https://github.com/windwp/nvim-ts-autotag/issues/125}
     },
-    cond = require("devstefancho.plugins_status").plugins_status["autotag"],
+    cond = require("devstefancho.plugins_status").plugins_status.autotag,
   },
 }
