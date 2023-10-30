25528ab43f8fe0454f14662ac8403af964b0c7e8
devstefancho <skku7714@gmail.com>
2023-10-30 23:46:51 +0900

    chore: upgrade dependencies

--- nvim/lua/devstefancho/plugins/telescope.lua
+++ nvim/lua/devstefancho/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   -- Telescope
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.1",
+    tag = "0.1.4",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-file-browser.nvim",
