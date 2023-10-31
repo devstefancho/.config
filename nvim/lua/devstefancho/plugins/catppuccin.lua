4bbc01c8fe615092a9bdfde18d40259e839d09e8
devstefancho <skku7714@gmail.com>
2023-10-31 23:38:47 +0900

    style: highlight Visual to be more visible

--- nvim/lua/devstefancho/plugins/catppuccin.lua
+++ nvim/lua/devstefancho/plugins/catppuccin.lua
@@ -3,6 +3,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      custom_highlights = function(colors)
+        return {
+          Visual = { bg = "#2d59a1" },
+        }
+      end,
+    })
     vim.cmd("colorscheme catppuccin")
   end,
   cond = require("devstefancho.plugins_status").plugins_status["catppuccin"],
