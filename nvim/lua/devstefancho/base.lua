c715a6f8818edd543c6e4571f6bf22b2d5be655e
devstefancho <skku7714@gmail.com>
2024-01-07 01:20:07 +0900

    config: set list visible

--- nvim/lua/devstefancho/base.lua
+++ nvim/lua/devstefancho/base.lua
@@ -27,7 +27,7 @@ vim.opt.inccommand = "split"
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.ai = true -- Auto indent
 vim.opt.si = true -- Smart indent
 vim.opt.wrap = false -- No wrap lines
@@ -36,6 +36,8 @@ vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
 vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.pumheight = 10
 vim.opt.ignorecase = true -- ignorecase for command mode autocompletion
+vim.opt.list = true
+vim.opt.listchars = { tab = "▸ ", trail = "·", extends = ">", precedes = "<" }
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
